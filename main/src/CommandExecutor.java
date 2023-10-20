b717b4219b7b06ca0e029e1714e7a5b5c362311c
filipporeucci <reu.filippo@gmail.com>
2023-10-20 17:39:09 +0200

    Scrittura su file csv distanza tra file aas presenti in una cartella

--- main/src/CommandExecutor.java
+++ main/src/CommandExecutor.java
@@ -1,27 +1,53 @@
+import java.io.*;
 import java.util.ArrayList;
 
 public class CommandExecutor {
-
     private final String file1;
-    public CommandExecutor(String file1) {
-        this.file1 = file1;
-    }
-    public ArrayList<String> commandBuilder(){
+
+    public CommandExecutor(String file1) {this.file1 = file1;}
+
+    public ArrayList<String> commandBuilder(String aasFilesPath, String stalignPath){
         ArrayList<String> comando = new ArrayList<>();
         comando.add("java");
         comando.add("-jar");
-        comando.add("/Users/diego/Downloads/STAlign-main/STAlign-main/download/STAlign.jar");
+        //comando.add("/Users/diego/Downloads/STAlign-main/STAlign-main/download/STAlign.jar");
+        //comando.add("/Users/filipporeucci/Desktop/STAlign-main/download/STAlign.jar"); //TODO CAMBIARE PATH
+        comando.add(stalignPath+"/STAlign.jar");
         comando.add("-edm");
-        comando.add(file1);
+        //comando.add("/Users/filipporeucci/Desktop/STAlign-main/examples/aas2/"+file1); //TODO CAMBIARE PATH
+        comando.add(aasFilesPath+"/"+file1);
         return comando;
     }
-    public void execCommand(String file2){
-        ArrayList<String> comando = commandBuilder();
-        comando.add(file2);
 
+    public void execCommand(String file2, File distanceFile, String aasFilesPath, String stalignPath){
+        ArrayList<String> comando = commandBuilder(aasFilesPath, stalignPath);
+        String distanceValue = "", linea = "";
+
+        //comando.add("/Users/filipporeucci/Desktop/STAlign-main/examples/aas2/"+file2); //TODO CAMBIARE PATH
+        comando.add(aasFilesPath+"/"+file2);
         ProcessBuilder processBuilder = new ProcessBuilder(comando);
+
         try {
             Process processo1 = processBuilder.start();
+            InputStream inputStream = processo1.getInputStream(); // Ottieni l'inputStream del processo
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream)); // Leggi l'output usando un BufferedReader
+
+            while ((linea = reader.readLine()) != null) {
+                System.out.println(linea);
+                if (linea.contains("Distance")) {
+                    String[] parts = linea.split("=");
+                    if (parts.length == 2)
+                        distanceValue = parts[1].trim();
+                }
+            }
+
+            try {
+                FileWriter writer = new FileWriter(distanceFile, true); //writing distance
+                writer.write(this.file1+"\t"+file2+"\t"+distanceValue+"\n");
+                writer.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
             processo1.waitFor(); // Attendere il completamento del processo (se necessario)
         } catch (Exception e) {
             System.out.println("Errore durante l'esecuzione del comando "+e);
--- main/src/Main.java
+++ main/src/Main.java
@@ -1,35 +1,47 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
-        String path = "/Users/diego/Downloads/siamobraviragazzi";
-        File pathInputDirectory = new File(path);
-
-        ArrayList<String> filesToBeAnalyze = new ArrayList<>();
-
-        File[] listOfFiles = pathInputDirectory.listFiles();
+        //String path = "/Users/diego/Downloads/siamobraviragazzi";
+        String aasFilesPath = "/Users/filipporeucci/Desktop/STAlign-main/examples/aas2"; //TODO CAMBIARE PATH
+        String stalignPath = "/Users/filipporeucci/Desktop/STAlign-main/download";
+        String distanceFileName = "StalignDistance.tsv";
+        File pathInputDirectory = new File(aasFilesPath);
+        File distanceFile = new File(aasFilesPath+"/"+distanceFileName);
+        ArrayList<String> filesToBeAnalyze = new ArrayList<>(); //aas files to be analyzed
+        File[] listOfFiles = pathInputDirectory.listFiles();  //files contained in the given directory
 
         if (listOfFiles != null) {
             for (File file : listOfFiles) {
-                if (file.isFile()) {
-                    filesToBeAnalyze.add(file.getName());
+                if (file.getName().compareTo(distanceFileName) == 0) { //if distance file already exists it will be deleted
+                    try {
+                        file.delete();
+                    } catch (SecurityException e) {
+                        System.err.println("Errore di sicurezza durante l'eliminazione del file: " + e.getMessage());
+                    }
                 }
+                if ((file.isFile()) && (file.getName().contains(".aas"))) filesToBeAnalyze.add(file.getName());
             }
-        } else {
-            System.out.println("Errore nella creazione della lista dei nomi dei file.");
+        } else System.out.println("Errore nella creazione della lista dei nomi dei file.");
+
+        try {
+            FileWriter writer = new FileWriter(distanceFile, true); //writing heading
+            writer.write("File1\tFile2\tDistance\n");
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        int i = 1;
-        //scorro la directory
+
         for (File file : listOfFiles) {
-            System.out.println("Sono al giro: "+i);
-            i++;
-            CommandExecutor command = new CommandExecutor(file.getName());
-            filesToBeAnalyze.remove(file.getName());
-            for (String fileN : filesToBeAnalyze) {
-                //System.out.println("Confronto file "+file.getName()+" con file"+fileN);
-                command.execCommand(fileN);
+            if(file.getName().contains(".aas")) {
+                CommandExecutor command = new CommandExecutor(file.getName());
+                filesToBeAnalyze.remove(file.getName());
+                for (String fileN : filesToBeAnalyze)
+                    if (fileN.contains(".aas")) command.execCommand(fileN, distanceFile, aasFilesPath, stalignPath);
             }
         }
     }
